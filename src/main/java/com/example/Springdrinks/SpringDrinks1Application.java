1883eae3868d9d2afeed0ad7ead59ddad77386b8
P4R5IVAL <abdimoha1123@gmail.com>
2022-01-09 15:46:53 +0000

    1st commit

---
+++ src/main/java/com/example/Springdrinks/SpringDrinks1Application.java
@@ -0,0 +1,13 @@
+package com.example.Springdrinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDrinks1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDrinks1Application.class, args);
+	}
+
+}
